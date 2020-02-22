14d9cec71d3a458309c0a7174cc58f6ec672a3a7
AlejoCasta <alejo.casta.al@gmail.com>
2020-02-22 06:50:40 -0500

    dojo apollo - graph

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const { ApolloServer } = require("apollo-server")
+
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const PostApi = require("./dataSource/postAPI");
+
+const server = new ApolloServer({typeDefs, resolvers, dataSources: () => ({PostApi: new PostApi()})});
+
+server.listen()
+.then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
